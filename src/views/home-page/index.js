3c5473a153ac99a75258735a1f507bccad7414da
Mateusz Szot <s181853@student.uek.krakow.pl>
2020-04-01 21:02:20 +0200

    initialize firebase app

--- src/views/home-page/index.js
+++ src/views/home-page/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FirebaseContext } from "../../context";
 
 const HomePage = () => {
+  const { app } = useContext(FirebaseContext);
+
+  // TODO: remove
+  console.log(app);
+
   return <h1>strona domowa</h1>;
 };
 
