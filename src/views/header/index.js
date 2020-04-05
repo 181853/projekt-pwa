b34f1bbac75d516e7c419800277e1f6ee9b07c40
Karolina-Pietryka <33173988+Karolina-Pietryka@users.noreply.github.com>
2020-04-05 16:32:43 +0200

    [PSM-2] Add basic navigation bar with footer to all pages (#6)
    * PWA-2 add basic layout with header and footer
    
    * PSM-2 add basic layout with header and footer

---
+++ src/views/header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../constants";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar className="bg-white mb-5" expand="sm">
+        <Navbar.Brand as={Link} to={ROUTES.HOME}>
+          Projekt PWA
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Button variant="outline-secondary" as={Link} to={ROUTES.LOGIN}>
+            Logowanie
+          </Button>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
--- src/views/index.js
+++ src/views/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./home-page";
 import { ROUTES } from "../constants";
+import Layout from "./layout";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path={ROUTES.HOME} component={HomePage} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path={ROUTES.HOME} component={HomePage} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 };
