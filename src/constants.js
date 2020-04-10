d68b19b5d2a116cc70397e120b704a0f0e453734
Mateusz Szot <s181853@student.uek.krakow.pl>
2020-04-10 21:50:16 +0200

    [PSM-5] Login - Google Authentication

--- src/constants.js
+++ src/constants.js
@@ -10,5 +10,6 @@ export const ERRORS = {
   "auth/invalid-email": "Nieprawidłowy adres email.",
   "auth/wrong-password": "Nieprawidłowe hasło.",
   "auth/email-already-in-use": "Użytkownik o podanym emailu już istnieje.",
-  "auth/user-not-found": "Użytkownik nie istnieje",
+  "auth/user-not-found": "Użytkownik nie istnieje.",
+  "auth/popup-closed-by-user": "Pop up zamknięty przez użytkownika.",
 };
