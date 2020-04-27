74d07784b88e081f3d7d61fe58a9b0b4627d6d7c
181853 <s181853@student.uek.krakow.pl>
2020-04-27 20:05:28 +0200

    [PSM-21] Missing favicon (#45)

--- src/views/header/index.js
+++ src/views/header/index.js
@@ -25,7 +25,7 @@ const Header = () => {
             height={30}
             className="d-inline-block align-top"
           />
-          {"// FIXME"}
+          {"//FIXME"}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
