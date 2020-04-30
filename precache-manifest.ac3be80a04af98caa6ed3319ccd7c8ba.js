eb79d1361d0b104cff04d63183cd981e9895cc13
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 19:23:40 +0000

    Deploy 181853/projekt-pwa to github.com/181853/projekt-pwa.git:gh-pages

---
+++ precache-manifest.ac3be80a04af98caa6ed3319ccd7c8ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb63d79f6d86b2002ecdf2ba5ed3683",
+    "url": "/projekt-pwa/index.html"
+  },
+  {
+    "revision": "4cccc4464bc85b9b355b",
+    "url": "/projekt-pwa/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c1b93c60012bac4fdd1e",
+    "url": "/projekt-pwa/static/css/3.b4a16772.chunk.css"
+  },
+  {
+    "revision": "9fdb44b73fa97a6e234b",
+    "url": "/projekt-pwa/static/css/main.1c50ccf2.chunk.css"
+  },
+  {
+    "revision": "4cccc4464bc85b9b355b",
+    "url": "/projekt-pwa/static/js/2.8e6b574b.chunk.js"
+  },
+  {
+    "revision": "befbf815f14b7a034abb54cdbf4aab13",
+    "url": "/projekt-pwa/static/js/2.8e6b574b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b93c60012bac4fdd1e",
+    "url": "/projekt-pwa/static/js/3.034f9c0f.chunk.js"
+  },
+  {
+    "revision": "46491ad3d927df2ea60d",
+    "url": "/projekt-pwa/static/js/4.0ce89480.chunk.js"
+  },
+  {
+    "revision": "9fdb44b73fa97a6e234b",
+    "url": "/projekt-pwa/static/js/main.4f263c1f.chunk.js"
+  },
+  {
+    "revision": "92766f7ec294ea33492b",
+    "url": "/projekt-pwa/static/js/runtime-main.ee516838.js"
+  }
+]);
